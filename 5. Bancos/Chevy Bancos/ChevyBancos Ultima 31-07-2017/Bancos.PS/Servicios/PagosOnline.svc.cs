bb54cd6b14a68a7b7e701da3c6401115161bc7c8
SAU95 <70765904+SAU95@users.noreply.github.com>
2021-04-15 12:18:18 -0500

    V0.2.4
    Ajuste a correos para que en pruebas no envíe nada, apuntamientos para pruebas

--- 5. Bancos/Chevy Bancos/ChevyBancos Ultima 31-07-2017/Bancos.PS/Servicios/PagosOnline.svc.cs
+++ 5. Bancos/Chevy Bancos/ChevyBancos Ultima 31-07-2017/Bancos.PS/Servicios/PagosOnline.svc.cs
@@ -206,8 +206,7 @@ namespace Bancos.PS.Servicios
                          CorreosNoti[0] = "marina.ramirez@Chevyplan.com.co";
 // Cambiar al pasar a produccion
                         //CorreosNoti[0] = "cristian.munoz@Chevyplan.com.co";
-                            Correo.enviarNotificaciones(Directorio, CorreosNoti , nombreArchivo, Remitente,
-                                                     registrosLote, tipoArchivo);
+                            //Correo.enviarNotificaciones(Directorio, CorreosNoti , nombreArchivo, Remitente,                                                     registrosLote, tipoArchivo);
 
                         ciclo = ciclo + Convert.ToInt16(LimitesSuperior[i]);
 
